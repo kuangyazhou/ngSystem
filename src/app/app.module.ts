0dc69cce8c0708737966fad0f1f6da9501efce16
况亚洲 <18707174713@139.com>
2018-09-04 16:44:20 +0800

    fuck angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { CoreModule } from "./core/core.module";
 
 import { AppComponent } from "./app.component";
 
+import { MatSidenavModule } from "@angular/material";
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    MatSidenavModule,
+    BrowserAnimationsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })

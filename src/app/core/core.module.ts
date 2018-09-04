6931dbfb8b591da854ed1cde9b00eaa52f1d1589
况亚洲 <18707174713@139.com>
2018-09-04 14:16:43 +0800

    布局

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,13 @@
 import { NgModule, SkipSelf, Optional } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
 
 @NgModule({
   imports: [CommonModule],
-  declarations: []
+  declarations: [HeaderComponent, FooterComponent, SidebarComponent],
+  exports: [HeaderComponent, FooterComponent, SidebarComponent]
 })
 export class CoreModule {
   // 如果没有，就正常注入，如果有，就报错
